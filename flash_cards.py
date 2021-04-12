23c22bb8aa6565ceef1ba9df405de5808bab1f0c
asadr98 <74267179+asadr98@users.noreply.github.com>
2021-04-12 14:30:09 -0400

    Add files via upload

---
+++ flash_cards.py
@@ -0,0 +1,27 @@
+class Cards:
+    """Reads in a file of questions and answers"""
+    
+    def __init__(self):
+    
+    
+    def shuffle():
+    
+    
+    def read_questions():
+        
+
+
+
+
+
+
+
+
+
+
+def main():
+    """Main function that calls classes and functions to run program."""
+   
+    
+if __name__ == "__main__":
+    main()
